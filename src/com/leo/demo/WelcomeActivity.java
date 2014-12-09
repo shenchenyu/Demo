4092ac3cb93630fe8c02c9d7bba393ec757e7be8
Leo-lay <499426493@qq.com>
2014-12-09 15:38:07 +0800

    test merge

--- src/com/leo/demo/WelcomeActivity.java
+++ src/com/leo/demo/WelcomeActivity.java
@@ -43,7 +43,7 @@ public class WelcomeActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.welcome_activity);
 		ViewUtils.inject(this);
 		init();
-//		----------------------------------------
+//		----------------leo------------------------
 	}
 	private void jumpOther(){
 		boolean flag = SharedPreferencesUtils.getBoolean(this, ContentValue.HAD_LOGIN, true);
