1bfdf7ede01e3bd8bd2e7b1020b7592b575ba18e
shenchenyu <android_chen992@163.com>
2014-12-09 17:17:01 +0800

    Revert "intent提交"
    This reverts commit 14747108a6f6c203e5af140e1d0c518a49c824fb.

--- src/com/leo/demo/WelcomeActivity.java
+++ src/com/leo/demo/WelcomeActivity.java
@@ -43,7 +43,7 @@ public class WelcomeActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.welcome_activity);
 		ViewUtils.inject(this);
 		init();
-
+//		----------------------------------------
 	}
 	private void jumpOther(){
 		boolean flag = SharedPreferencesUtils.getBoolean(this, ContentValue.HAD_LOGIN, true);
@@ -80,8 +80,7 @@ public class WelcomeActivity extends Activity implements OnClickListener {
 					int position, long id) {
 				switch (position) {
 				case 0:
-//					Intent intent = new Intent(ct,InvoiceActivity.class);
-					startActivity(intent);
+					PromptManager.showToast(ct, R.string.gv_txt_1);
 					break;
 				case 1:
 					PromptManager.showToast(ct, R.string.gv_txt_2);
